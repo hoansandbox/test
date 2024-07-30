b20e7be88a34edb54d209e06d2e82b177465f1c0
hoansandbox <hoan.sandbox@gmail.com>
2024-07-30 16:13:35 +0700

    Update plyr-custom.js

--- plyr-custom.js
+++ plyr-custom.js
@@ -1,5 +1,6 @@
-setTimeout(function(){
-  document.querySelectorAll('.plyr__video-embed').forEach(function(el) {
-  	document.querySelector('.plyr__video-wrapper').insertAdjacentHTML('beforeend','<div class="plyr__custom-overlay"></div>');
-  })
-}, 300)
+document.querySelectorAll('.plyr__video-embed').forEach(function(el) {
+	let player = new Plyr(el);
+	player.on('ready', (event) => {
+	  document.querySelector('.plyr__video-wrapper').insertAdjacentHTML('beforeend','<div class="plyr__custom-overlay"></div>')
+	});
+})
