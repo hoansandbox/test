98cc967472fe6660a624a79ce73a5fa8c1a3cccd
hoansandbox <hoan.sandbox@gmail.com>
2024-07-30 16:32:29 +0700

    Update plyr-custom.js

--- plyr-custom.js
+++ plyr-custom.js
@@ -1,4 +1,5 @@
-document.querySelectorAll('.plyr__video-embed').forEach(function(el) {
+document.querySelectorAll('.plyr-video').forEach(function(el) {
+	el.classList.add('plyr__video-embed');
 	let player = new Plyr(el);
 	player.on('ready', (event) => {
 	  document.querySelector('.plyr__video-wrapper').insertAdjacentHTML('beforeend','<div class="plyr__custom-overlay"></div>')
